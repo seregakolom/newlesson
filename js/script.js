742c1bb71621ea1f8dcba389079b2b7999f45a89
Seregakolom <seregakolom@gmail.com>
2021-11-29 15:39:11 +0200

    commit test

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*let number = 5;
+console.log(number);
+
+const person = `Alex`;
+console.log(person);
+
+const obj  = {
+    name: "Sergey",
+    age: 25,
+};
+console.log(obj.age);
+
+alert("Привет");*/
+
+
